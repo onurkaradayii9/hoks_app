38186c9d75bf21ed7ba3311ee446faf586487204
onurkaradayii9 <onurkaradayi9@gmail.com>
2025-11-07 02:35:09 +0300

    Add files via upload

---
+++ hok_regl_full_ready/hok_regl_android_fixed/android/app/src/main/kotlin/com/example/hok_regl/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hok_regl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hok_regl_full_ready/hok_regl_android_fixed/android/gradlew.bat
@@ -0,0 +1,2 @@
+REM Placeholder gradlew.bat
+@echo off
