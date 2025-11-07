99b48ffcacc530a44cf6007e9a1f3b8e29c7f7d0
onurkaradayii9 <onurkaradayi9@gmail.com>
2025-11-07 03:47:32 +0300

    Update gradlew.bat with proper initialization

--- hok_regl_full_ready/hok_regl_android_fixed/android/gradlew.bat
+++ hok_regl_full_ready/hok_regl_android_fixed/android/gradlew.bat
@@ -1,2 +1,8 @@
-REM Placeholder gradlew.bat
-@echo off
+@ECHO OFF
+SETLOCAL
+SET DIRNAME=%~dp0
+SET APP_BASE_NAME=%~n0
+SET APP_HOME=%DIRNAME%
+SET DEFAULT_JVM_OPTS=
+
+java %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -cp "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
